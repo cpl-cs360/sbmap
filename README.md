f27bd69e03a63e0d9b8db88e497df681a99f694d
colmpat <cplang@dons.usfca.edu>
2022-03-12 16:16:31 -0800

    React

--- README.md
+++
@@ -1,4 +0,0 @@
-> Colm Lang's Final Project for CS360 Data Visualization @[USFCA](https://www.usfca.edu)
-
-# SBMAP
-The **S**mall **B**ody **M**ap for the **A**verage **P**erson
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './app.scss'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>The Small Body Map for the Average Person</h1>
+      <h2><em>SBMAP</em>. CS360 Final Project</h2>
+      <h3>Colm Lang</h3>
+    </div>
+  )
+}
+
+export default App
