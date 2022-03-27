47f77a8c1508083dfa55377269f1abf36cee9cd9
colmpat <cplang@dons.usfca.edu>
2022-03-27 01:04:58 -0700

    Added first iteration of the Hexbin Viz

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './app.scss'
-import BarChart from './components/BarChart'
+import HexBin from './components/hexbin/HexBin';
+import * as d3 from 'd3';
 
 function App() {
-  const data = [1, 2, 3, 4, 7, 10, 15, 16, 19];
-  const barChartDimensions = {
-    width: 400,
-    height: 350,
+
+  const [hexBinData, setHexBinData] = useState();
+  const hexBinDims = {
+    width: 1000,
+    height: 1000,
     margin: {
-      top: 100,
-      right: 75,
-      bottom: 100,
-      left: 75
+      top: 10,
+      right: 10,
+      bottom: 10,
+      left: 10,
     }
   };
+
+  useEffect(() => {
+
+    // load in hexbin csv
+    d3.csv('../data/hex_bins.csv', d => {
+      return {
+        x: +d['x'],
+        y: +d['y'],
+        count: +d['count'],
+      }
+    }).then(data => {
+      // console.table(data);
+      setHexBinData(data);
+    })
+  
+  }, [])
+
+  console.log(hexBinData)
 
   return (
     <>
@@ -21,9 +41,9 @@ function App() {
         <h1>The Small Body Map for the Average Person</h1>
         <h2><em>SBMAP</em>. CS360 Final Project</h2>
         <h3>Colm Lang</h3>
-        <p>Welcome to SBMAP,<br/>I hope this interactive visualization will teach you something about our solar system. It is my goal to bridge the gap between Astrophysicists and <em>the rest of us</em>. You can find the dataset that i used <a href='www.google.com'>here</a>.</p>
+        <p>Welcome to SBMAP,<br/>I hope this interactive visualization will teach you something about our solar system. It is my goal to bridge the gap between Astrophysicists and <em>the rest of us</em>. You can find the dataset that I used <a href='https://ssd.jpl.nasa.gov/tools/sbdb_query.html'>here</a>.</p>
       </div>
-      <BarChart data={data} dimensions={barChartDimensions} />
+      <HexBin data={ hexBinData } dimensions={ hexBinDims } />
     </>
   )
 }
---
+++ src/components/hexbin/HexBin.jsx
@@ -0,0 +1,51 @@
+import React, { useRef, useEffect } from 'react'
+import * as d3 from 'd3';
+import './hexBin.scss'
+
+export default function HexBin({ data, dimensions }) {
+    const svgRef = useRef(null);
+    const { width, height, margin } = dimensions;
+    const svgWidth = width + margin.left + margin.right;
+    const svgHeight = height + margin.top + margin.bottom;
+
+    //useEffect to handle D3 related side-effects (like DOM manipulation)
+    useEffect(() => {
+
+        if(data == null) return;
+        const svgRefElement = d3.select(svgRef.current);
+        svgRefElement.selectAll("*").remove()   //clear svg content before (re)drawing
+        
+        const maxCount = d3.max(data.map(d => d.count));
+        const colorScale = d3.interpolateInferno;
+
+        const svg = svgRefElement
+            .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+        
+        svg.selectAll('circle')
+            .data(data)
+            .enter()
+            .append('polygon')
+            .attr('points', d => {
+                // given a radius of 5 pixels, a hexagon can be thought of as a function of its 30 60 90 triangle
+                // if b = 5, then a = b / root3, and c = 2a
+                let b = 5;
+                let a = b / Math.sqrt(3);
+                let c = 2 * a;
+                return [
+                    { x: d.x + b, y: d.y + a},      // top right
+                    { x: d.x + b, y: d.y - a},      // bottom right
+                    { x: d.x, y: d.y - c},          // bottom center
+                    { x: d.x - b, y: d.y - a},      // bottom left
+                    { x: d.x - b, y: d.y + a},      // top left
+                    { x: d.x, y: d.y + c},          // top center
+                ].map(p => `${ p.x }, ${ p.y }`).join(' ')
+            })
+            .attr('fill', d => colorScale(Math.log(d.count) / Math.log(maxCount)));
+            
+    }, [data]); //redraw chart when data changes
+    
+    return (
+        <svg ref={svgRef} width={svgWidth} height={svgHeight} className="hexBin"/>
+    )
+}
