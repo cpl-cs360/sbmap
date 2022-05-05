97e11b3f2654e5ef1b7b04febd9de810a432b7c2
colmpat <cplang@dons.usfca.edu>
2022-05-05 16:11:40 -0700

    Updated intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import * as d3 from 'd3';
-import ViolinPlot from '../violin-plot/ViolinPlot';
 import './intro.scss';
+import ArrowDownwardRoundedIcon from '@mui/icons-material/ArrowDownwardRounded';
 
 export default function Intro() {
   const [data, setData] = useState();
@@ -34,13 +34,25 @@ export default function Intro() {
   return (
     <div className="intro">
       <div className="introWrapper">
-        <div>
-          <h1>The Small Body Map for the Average Person</h1>
-          <h2><em>SBMAP</em>. CS360 Final Project</h2>
-          <h3>Colm Lang</h3>
-          <p>Welcome to SBMAP,<br/>I hope this interactive visualization will teach you something about our solar system. It is my goal to bridge the gap between Astrophysicists and <em>the rest of us</em>. You can find the dataset that I used <a href='https://ssd.jpl.nasa.gov/tools/sbdb_query.html'>here</a>.</p>
+        <h1>The Small Body Map for the Average Person</h1>
+        <h2><em>SBMAP</em>. CS360 Final Project</h2>
+        <h3>Colm Lang</h3>
+        <hr/>
+        <div className='bodyText'>
+          <p>Welcome to SBMAP,</p>
+          <p>SBMAP aims to provide a digestible, holistic representation of our solar system's many small bodies without sacrificing detail or accuracy. I hope this interactive visualization will teach you something about our solar system. It is my goal to bridge the gap between Astrophysicists and <em>the rest of us</em>.</p>
+          <p><u>Note:</u> You can find the dataset that I used <a href='https://ssd.jpl.nasa.gov/tools/sbdb_query.html'>here</a>. Best viewed on Opera or Firefox with resolution higher than 1280x800.</p>
         </div>
-        <ViolinPlot data={data} dimensions={violinPlotDimensions} />
+        
+        {/* <ViolinPlot data={data} dimensions={violinPlotDimensions} /> */}
+      </div>
+      <div className='arrow'>
+        <ArrowDownwardRoundedIcon className='arrowIcon'/>
+      </div>
+      <div className='links'>
+        <a href="https://github.com/cpl-cs360/sbmap" target='_blank' className='button'>Github</a>
+        <a href="https://github.com/cpl-cs360/sbmap#sbmap" target='_blank' className='button'>Docs</a>
+        <a href="/sbmap/AlphaRelease.pdf" target='_blank' className='button'>Alpha Release</a>
       </div>
     </div>
   )
